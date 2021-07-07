17a75898c67fdfcaddad34684d1b97b8345176e4
AbhidhaChoudhari <86190226+AbhidhaChoudhari@users.noreply.github.com>
2021-07-07 15:07:01 +0530

    Add files via upload
    add

---
+++ 3. IMPLEMENTATION/MULTIFILE/displayOrder.c
@@ -0,0 +1,13 @@
+#include "foodfun.h"
+
+int displayOrder(){
+    int i=0;
+    printf("\n     Your Final Order     \n");
+    while(strlen(order[i])!=0){
+        printf("item = %s, quantity = %s\n",order[i],order[i+1]);
+        i = i+2;
+    }
+    printf("Total Price = %d",totalPrice);
+    printf("\n\n\nThank you for visiting our restaurant");
+    return 0;
+}
