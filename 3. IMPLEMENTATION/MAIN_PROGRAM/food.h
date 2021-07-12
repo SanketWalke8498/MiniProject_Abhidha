7cb91ffd9ebe1f7cd87689d7ac443584c0aae263
AbhidhaChoudhari <86190226+AbhidhaChoudhari@users.noreply.github.com>
2021-07-12 18:32:47 +0530

    Add files via upload
    added

---
+++ 3. IMPLEMENTATION/MAIN_PROGRAM/food.h
@@ -0,0 +1,11 @@
+#ifndef __FOOD_H 
+#define __FOOD_H
+/**
+ * @brief calling function
+ * 
+ * @return int 
+ */
+int displayOrder();
+int refreshOrderlist();
+
+#endif
---
+++ 3. IMPLEMENTATION/MAIN_PROGRAM/refresh.c
@@ -0,0 +1,20 @@
+#include "food.h" /**
+ * @brief header gaurd
+ * 
+ * @return int 
+ */
+
+
+int refreshOrderlist(){
+    int i=0,j;
+    char order[10][20]={""};
+    int totalPrice;
+    while(strlen(order[i])!=0){
+        strcpy(order[i],"xyz");
+        i++;
+    }
+    j = 0;
+    totalPrice = 0;
+
+   
+}
