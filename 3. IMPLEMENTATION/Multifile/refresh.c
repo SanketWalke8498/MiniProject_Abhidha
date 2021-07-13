8fa5fae67df5b04a5dde3a7f55002f704c0e3850
AbhidhaChoudhari <86190226+AbhidhaChoudhari@users.noreply.github.com>
2021-07-13 09:48:05 +0530

    Add files via upload
    add

---
+++ 3. IMPLEMENTATION/Multifile/refresh.c
@@ -0,0 +1,18 @@
+#include "food.h" /**
+ * @brief header gaurd
+ * 
+ * @return int 
+ */
+
+
+int refreshOrderlist(){
+    int i=0;
+    char order[10][20]={""};
+    int totalPrice;
+    while(strlen(order[i])!=0){
+        strcpy(order[i],"xyz");
+        i++;
+    }
+    
+   
+}
